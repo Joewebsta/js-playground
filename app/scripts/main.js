c70154fe1dc26951c0894db05d67ffb01585632b
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-28 18:46:11 -0400

    Created a dog constructor to create two dog instances

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,31 @@
-var calc = {
-  status : "Awesome",
-  plus : function(a,b) {
-    console.log(this);
-    console.log(a+b);
-    console.log(arguments);
-    console.log(this.status);
-  }
-}
+'use strict';
 
-calc.plus(2,2);
+// 2 *****************
+
+var Dog = function() {
+    var name, breed;
+    console.dir(this);
+};
+
+var firstDog = new Dog;
+    firstDog.name = 'Molly';
+    firstDog.breed = 'Pitbull';
+
+var secondDog = new Dog;
+    secondDog.name = "Max";
+    secondDog.breed = "Husky";
+
+
+// 1 *****************
+
+// var calc = {
+//   status : "Awesome",
+//   plus : function(a,b) {
+//     console.log(this);
+//     console.log(a+b);
+//     console.log(arguments);
+//     console.log(this.status);
+//   }
+// }
+
+// calc.plus(2,2);
