885e318e3931b58c9bf28bc8a983f535b8f1b50a
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-29 21:41:09 -0400

    Experiments with chaining module method calls

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,8 @@
 var joe = (function(){
 
   var DEFAULTS = {
-    say : 'hello'
+    say : 'hello',
+    speed : 'normal'
   };  
 
   return {
@@ -13,6 +14,14 @@ var joe = (function(){
       var myArguments = arguments[0] || '';
       var statement = myArguments.say || DEFAULTS.say;
       console.log(statement);
+      return this;
+    },
+
+    run : function() {
+      var myArguments = arguments[0] || '';
+      var running = myArguments.speed || DEFAULTS.speed;
+      console.log('running... ' + running);
+      return this;
     }
   }
 
